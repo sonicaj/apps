1f1552672a72286d02075873c6fc1453a00cb7ef
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-05-10 00:04:12 +0500

    Remove unused trains for now

--- trains/charts/nginx/1.0.0/templates/library/charts/nginx/v1_0_0/utils.py
+++
@@ -1,2 +0,0 @@
-def port_binding(input_port, output_port):
-    return f'0.0.0.0:{input_port}:{output_port}'
--- trains/charts/pihole/2.0.10/templates/library/charts/pihole/v2_0_10/images.py
+++
@@ -1,7 +0,0 @@
-from .utils import port_binding
-
-def pihole_image_name(tag):
-    return f'pihole:{tag}'
-
-def port_to_bind(port):
-    return port_binding(port, port)
