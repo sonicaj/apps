8d3f6c60b3731eec6e35ec8812600aa529e90b03
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-05-10 02:15:48 +0500

    Add an example importing stuff from base lib

--- ix-dev/charts/nginx/templates/library/charts/nginx/v1_0_1/images.py
+++ ix-dev/charts/nginx/templates/library/charts/nginx/v1_0_1/images.py
@@ -1,7 +1,10 @@
+from base_v1_0_0.containers import container_name
 from .utils import port_binding
 
+
 def nginx_image_name(tag):
-    return f'nginx:{tag}'
+    return f'nginx:{tag}:{container_name("testing")}'
+
 
 def port_to_bind(port):
     return port_binding(port, port)
